49163cef770fab193d96f2586466257c45b37eff
Manu <ominuka.mauro@gmail.com>
2022-10-15 16:21:04 -0300

    Refactor: Added navbar to App and Outlet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import ChatWindow from './components/ChatWindow'
 import { Container } from '@mui/material'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <Container>
-      <ChatWindow />
+      <Navbar />
+      <Outlet />
     </Container>
   )
 }
