07c52195ef05be3fb8f3dfeb1dbf42edcadff8be
Manu <ominuka.mauro@gmail.com>
2022-10-17 13:46:49 -0300

    Add state for rooms and dynamic create room

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,68 @@
-import { Card, Button, Typography } from '@mui/material'
-import { Link, Navigate, Outlet } from 'react-router-dom'
+import { Card, Button, Typography, Box } from '@mui/material'
+import {
+  Link,
+  Navigate,
+  Outlet,
+  useNavigate,
+  useOutletContext,
+} from 'react-router-dom'
 import { v4 } from 'uuid'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 const Navbar = ({ socket }) => {
-  let roomId
+  const [rooms, setRooms] = useState([])
+
+  const navigate = useNavigate()
+
   const createNewRoom = () => {
-    roomId = v4()
-    Navigate(`/room/${roomId}`)
+    const roomId = v4()
+    navigate(`/room/${roomId}`)
     socket.emit('new-room-created', { roomId })
+    socket.emit('new-room-created', roomId)
+    setRooms([...rooms, roomId])
   }
 
   useEffect(() => {
     if (!socket) return
+    socket.on('new-room-created', ({ roomId }) => {
+      setRooms([...rooms, roomId])
+    })
   }, [socket])
+
   return (
     <Card sx={{ marginTop: '10px', backgroundColor: 'gray' }} raised>
-      <Link style={{ textDecoration: 'none' }} to="/">
-        <Button sx={{ color: 'white', textDecoration: 'none' }} variant="text">
-          Home
-        </Button>
-      </Link>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box>
+          <Link style={{ textDecoration: 'none' }} to="/">
+            <Button
+              sx={{ color: 'white', textDecoration: 'none' }}
+              variant="text"
+            >
+              Home
+            </Button>
+          </Link>
+        </Box>
 
-      <Link style={{ textDecoration: 'none' }} to={`/room/${roomId}`}>
-        <Button sx={{ color: 'white', textDecoration: 'none' }} variant="text">
-          Salas
+        {rooms.map((room) => (
+          <Link
+            style={{ color: 'white', textDecoration: 'none' }}
+            to={`/room/${room}`}
+          >
+            <Button
+              sx={{ color: 'white', textDecoration: 'none' }}
+              variant="text"
+            >
+              {room}
+            </Button>
+          </Link>
+        ))}
+        <Button
+          sx={{ color: 'white', textDecoration: 'none' }}
+          onClick={createNewRoom}
+          variant="text"
+        >
+          New Room
         </Button>
-      </Link>
+      </Box>
     </Card>
   )
 }
