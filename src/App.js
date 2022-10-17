504b7794612df83cb801fdae2ea79495c2c76c8a
Manu <ominuka.mauro@gmail.com>
2022-10-17 10:53:05 -0300

    Set connection with server-socket

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Container } from '@mui/material'
+import { Box, Container } from '@mui/material'
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { io } from 'socket.io-client'
 import Navbar from './components/Navbar'
+
 function App() {
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    setSocket(io('http://localhost:5000'))
+  }, [])
+  let userId = '12asdfs'
   return (
     <Container>
-      <Navbar />
-      <Outlet />
+      <Navbar socket={socket} />
+      <Box>
+        <Outlet context={{ socket, userId }} />
+      </Box>
     </Container>
   )
 }
